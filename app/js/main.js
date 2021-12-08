ed23ba3d43b7c70ac57bdb0cc9de02a2249252e5
Valeriy <leshikzoloto@gmail.com>
2021-12-08 18:51:00 +0300

    start popups in header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,19 @@
-$(function () {});
+$(function () {
+	//Popup Как нам связатся
+	$(".open-popup").click(function (e) {
+		e.preventDefault();
+		$(".popup-bg").fadeIn(600);
+		$("html").addClass("no-scroll");
+	});
+	$(".close-popup").click(function () {
+		$(".popup-bg").fadeOut(600);
+		$("html").removeClass("no-scroll");
+	});
+	$(".popup-bg").click(function () {
+		$(".popup-bg").fadeOut(600);
+		$("html").removeClass("no-scroll");
+	});
+	$(".popup").on("click", function (e) {
+		e.stopPropagation();
+	});
+});
